75e252ce4f655cb0576bcef0a81a388bf828efcf
tboulet <timothe.boulet0@gmail.com>
2024-01-25 16:40:23 +0100

    change names

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -2,9 +2,9 @@ from typing import Dict, Type
 
 from algorithms.base_nfg_algorithm import BaseNFGAlgorithm
 from algorithms.forel import Forel
-from algorithms.iterated_forel import IteratedForel
+from algorithms.iterated_forel_lyapunov import IteratedForel
 
 algo_name_to_nfg_solver : Dict[str, Type[BaseNFGAlgorithm]] = {
-    "iterated_forel" : IteratedForel,
+    "iterated_forel_lyapunov" : IteratedForel,
     "forel" : Forel,
 }
