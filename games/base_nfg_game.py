5418badf8f3871e256c8f3847c92a0eef2a35238
clementw168 <clementwang2001@gmail.com>
2024-02-08 10:27:17 +0100

    feat: nash conv for kuhn poker

--- games/base_nfg_game.py
+++ games/base_nfg_game.py
@@ -1,23 +1,28 @@
 from abc import ABC, abstractmethod
 from typing import List
+import numpy as np
 
 from core.typing import JointPolicy, Policy
 
 
 class BaseNFGGame(ABC):
-    """The base class for any Normal Form Game"""    
-    
+    """The base class for any Normal Form Game"""
+
     @abstractmethod
-    def get_rewards(self, joint_action : List[int]) -> List[float]:
+    def get_rewards(self, joint_action: List[int]) -> List[float]:
         """Return the rewards for each player"""
         pass
-    
+
     @abstractmethod
     def num_distinct_actions(self) -> int:
         """Return the number of distinct actions"""
         pass
-    
+
     @abstractmethod
     def num_players(self) -> int:
         """Return the number of players"""
         pass
+
+    def get_utility_matrix(self) -> np.ndarray:
+        """Return the utility matrix"""
+        raise NotImplementedError("This game does not have a utility matrix")
