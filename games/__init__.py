1f7501c7762a21c6288f31cfffdcc6a90b9cd9bc
tboulet <timothe.boulet0@gmail.com>
2024-02-19 02:03:42 +0100

    Add Rock Paper Scissor

--- games/__init__.py
+++ games/__init__.py
@@ -3,8 +3,10 @@ from typing import Dict, Type
 from games.base_nfg_game import BaseNFGGame
 from games.kuhn_poker import KuhnPokerNFG
 from games.matching_pennies import MatchingPennies
+from games.rock_paper_scissor import RockPaperScissor
 
 game_name_to_nfg_solver: Dict[str, Type[BaseNFGGame]] = {
     "matching_pennies": MatchingPennies,
     "kuhn_poker": KuhnPokerNFG,
+    "rock_paper_scissor": RockPaperScissor,
 }
