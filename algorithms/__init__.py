4cd294ee2cf3074d4211f1adf4caf2aa0938fa4a
tboulet <timothe.boulet0@gmail.com>
2024-03-07 16:08:45 +0100

    add PIL FoReL

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -5,10 +5,12 @@ from algorithms.forel import Forel
 from algorithms.iterated_forel_lyapunov import IteratedForel
 from algorithms.policy_gradient import PolicyGradient
 from algorithms.population_forel import PopulationForel
+from algorithms.population_iterated_lyapunov_forel import PopulationIteratedLyapunovForel
 
 algo_name_to_nfg_solver: Dict[str, Type[BaseNFGAlgorithm]] = {
     "policy_gradient": PolicyGradient,
     "iterated_forel_lyapunov": IteratedForel,
     "forel": Forel,
     "population_forel": PopulationForel,
+    "PIL_FoReL" : PopulationIteratedLyapunovForel,
 }
